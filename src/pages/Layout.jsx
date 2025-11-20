ef9d05f660e9fb8f626d7482e935456c2746d022
Bhushan Nasre <bhushannasre3@gmail.com>
2025-11-21 00:30:52 +0530

    third commit

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
-
+import Navbar from "../components/Navbar";
 function Layout(){
     return (
         <div>
-            <div>
+            <div className="min-h-screen bg-gray-50">
+                <Navbar/>
                 <Outlet/>
             </div>
         </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,13 @@
 import { User2Icon } from "lucide-react";
 import { useState } from "react";
 import { Mail } from "lucide-react";
+import { Lock } from "lucide-react";
+
 
 function Login(){
-      const [state, setState] = useState("login")
+    const query = new URLSearchParams(window.location.search);
+    const urlState= query.get('state');
+      const [state, setState] = useState(urlState || "login")
 
     const [formData, setFormData] = useState({
         name: '',
@@ -21,8 +25,8 @@ function Login(){
         setFormData(prev => ({ ...prev, [name]: value }))
     }
     return (
-        
-             <form onSubmit={handleSubmit} className="sm:w-[350px] w-full text-center border border-gray-300/60 rounded-2xl px-8 bg-white">
+        <div className="flex flex-center justify-center min-h-screen bg-gray-100 ">
+             <form onSubmit={handleSubmit} className="sm:w-[350px] w-full h-120 text-center border border-gray-300/60 rounded-2xl px-8 bg-white">
                 <h1 className="text-gray-900 text-3xl mt-10 font-medium">{state === "login" ? "Login" : "Sign up"}</h1>
                 <p className="text-gray-500 text-sm mt-2">Please {state} to continue</p>
                 {state !== "login" && (
@@ -36,17 +40,19 @@ function Login(){
                     <input type="email" name="email" placeholder="Email id" className="border-none outline-none ring-0" value={formData.email} onChange={handleChange} required />
                 </div>
                 <div className="flex items-center mt-4 w-full bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="#6B7280" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-lock-icon lucide-lock"><rect width="18" height="11" x="3" y="11" rx="2" ry="2" /><path d="M7 11V7a5 5 0 0 1 10 0v4" /></svg>
+                    <Lock size={13} color="#6B7280"/>
                     <input type="password" name="password" placeholder="Password" className="border-none outline-none ring-0" value={formData.password} onChange={handleChange} required />
                 </div>
-                <div className="mt-4 text-left text-indigo-500">
+                <div className="mt-4 text-left text-green-500">
                     <button className="text-sm" type="reset">Forget password?</button>
                 </div>
-                <button type="submit" className="mt-2 w-full h-11 rounded-full text-white bg-indigo-500 hover:opacity-90 transition-opacity">
+                <button type="submit" className="mt-2 w-full h-11 rounded-full text-white bg-green-500 hover:opacity-90 transition-opacity">
                     {state === "login" ? "Login" : "Sign up"}
                 </button>
-                <p onClick={() => setState(prev => prev === "login" ? "register" : "login")} className="text-gray-500 text-sm mt-3 mb-11">{state === "login" ? "Don't have an account?" : "Already have an account?"} <a href="#" className="text-indigo-500 hover:underline">click here</a></p>
+                <p onClick={() => setState(prev => prev === "login" ? "register" : "login")} className="text-gray-500 text-sm mt-3 mb-11">{state === "login" ? "Don't have an account?" : "Already have an account?"} <a href="#" className="text-green-500 hover:underline">click here</a></p>
             </form>
+        </div>
+            
         
     )
 
